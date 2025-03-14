cbde71d4e1f9589f0cdbbcf92a82edd7711eb5f3
MaksLevchenko <111285984+MaksLevchenko@users.noreply.github.com>
2025-03-15 00:57:39 +0300

    Немного подредактировал файл logic.py

--- store_telega/logic/logic.py
+++ store_telega/logic/logic.py
@@ -1,13 +1,37 @@
 import time
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from bot import all_media_dir
 from store_telega.crud import add_data, get_data
 
 
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=options)
+
+
+def parse_page(url: str, xpath: str) -> tuple[str, float]:
+    """
+    Функция парсит страницу и возвращает цену товара
+    """
+    driver.get(url)
+    wait = WebDriverWait(driver, 20)
+    element = wait.until(
+        EC.presence_of_element_located(
+            (
+                By.XPATH,
+                xpath.split("/")[:-1] if xpath.endswith("/text()") else xpath,
+            )
+        )
+    )
+    price = price_is_digit(element.text)
+    return price
 
 
 async def pars_file(filename: str):
@@ -15,7 +39,7 @@ async def pars_file(filename: str):
     Функция парсит конфигурационный файл и возвращает словарь с данными
     """
     # Выполняем чтение файла
-    data = pd.read_excel(f"{all_media_dir}/{filename}")
+    data = pd.read_excel(f"{all_media_dir}/{filename}", engine="openpyxl")
 
     data_2 = []
     # Преобразуем полученные данные в словарь
@@ -23,13 +47,8 @@ async def pars_file(filename: str):
         title = i["title"]
         url = i["url"]
         xpath = i["xpath"]
-        driver.get(url)
-        time.sleep(10)
-        elem = driver.find_element(
-            By.XPATH, xpath.split("/")[:-1] if xpath.endswith("/text()") else xpath
-        )
 
-        price = price_is_digit(str(elem.text))
+        price = parse_page(url, xpath)
 
         if price:
             data_2.append(
@@ -76,10 +95,7 @@ def price_is_digit(price: str) -> None | float:
     Функция проверяет, является ли цена числом
     """
     new_price = ""
-    for i in price.split():
-        for j in i:
-            if j.isdigit():
-                new_price += j
-    if new_price.isdigit():
+    new_price = "".join([c for c in price if c.isdigit() or c == "." or c == ","])
+    if new_price:
         return float(new_price)
     return None
